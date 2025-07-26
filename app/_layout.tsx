b478b9efb317678fd4172a80b6e17010dcb4257f
asisaaa <105841105222@student.unismuh.ac.id>
2025-07-26 22:35:12 +0700

    Update tugas lab 4

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,16 +7,16 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    "barlow-extrabold": require("../assets/fonts/Barlow-ExtraBold.ttf"),
-    "bitcount": require("../assets/fonts/Bitcount-VariableFont_CRSV,ELSH,ELXP,slnt,wght.ttf"),
-    "bitcount-prop": require("../assets/fonts/BitcountPropSingle-VariableFont_CRSV,ELSH,ELXP,slnt,wght.ttf"),
-    "lemon": require("../assets/fonts/Lemon-Regular.ttf"),
-    "libertinus": require("../assets/fonts/LibertinusMono-Regular.ttf"),
-    "oswald": require("../assets/fonts/Oswald-VariableFont_wght.ttf"),
-    "roboto": require("../assets/fonts/Roboto-VariableFont_wdth,wght.ttf"),
-    "roboto-condensed": require("../assets/fonts/RobotoCondensed-Italic-VariableFont_wght.ttf"),
-    "special-gothic": require("../assets/fonts/SpecialGothicExpandedOne-Regular.ttf"),
-    "ubuntu": require("../assets/fonts/Ubuntu-Regular.ttf"),
+    "bigshoulders": require("../assets/fonts/BigShouldersInline-VariableFont_opsz,wght.ttf"),
+    "darkergrotesque": require("../assets/fonts/DarkerGrotesque-VariableFont_wght.ttf"),
+    "eduhand": require("../assets/fonts/EduAUVICWANTHand-VariableFont_wght.ttf"),
+    "inriasans": require("../assets/fonts/InriaSans-BoldItalic.ttf"),
+    "playwrite": require("../assets/fonts/PlaywriteNGModernGuides-Regular.ttf"),
+    "simonetta": require("../assets/fonts/Simonetta-Regular.ttf"),
+    "sourgummy": require("../assets/fonts/SourGummy-Italic-VariableFont_wdth,wght.ttf"),
+    "sunshiney": require("../assets/fonts/Sunshiney-Regular.ttf"),
+    "girlnextdoor": require("../assets/fonts/TheGirlNextDoor-Regular.ttf"),
+    "tiltprism": require("../assets/fonts/TiltPrism-Regular-VariableFont_XROT,YROT.ttf"),
   });
 
   useEffect(() => {
--- app/index.tsx
+++ app/index.tsx
@@ -4,44 +4,44 @@ export default function Index() {
   return (
     <ScrollView contentContainerStyle={{ padding: 20 }}>
 
-      <Text style={{ fontFamily: "barlow-extrabold", fontSize: 24 }}>
+      <Text style={{ fontFamily: "bigshoulders", fontSize: 24 }}>
         Diah Rahmatillah{"\n"}105841104822
       </Text>
 
-      <Text style={{ fontFamily: "bitcount", fontSize: 24 }}>
+      <Text style={{ fontFamily: "darkergrotesque", fontSize: 24 }}>
         Juliani{"\n"}105841104922
       </Text>
 
-      <Text style={{ fontFamily: "bitcount-prop", fontSize: 24 }}>
+      <Text style={{ fontFamily: "eduhand", fontSize: 24 }}>
         Azzah Aulia Syarif{"\n"}105841105022
       </Text>
 
-      <Text style={{ fontFamily: "lemon", fontSize: 24 }}>
-        Syauqiah Mujahidah Yahya{"\n"}105841105122
+      <Text style={{ fontFamily: "inriasans", fontSize: 24 }}>
+        Syauqiyah Mujahidah Yahya{"\n"}105841105122
       </Text>
 
-      <Text style={{ fontFamily: "libertinus", fontSize: 24 }}>
-        Mar atul azizah{"\n"}105841105222
+      <Text style={{ fontFamily: "playwrite", fontSize: 24 }}>
+        Mar'atul Azizah{"\n"}105841105222
       </Text>
 
-      <Text style={{ fontFamily: "oswald", fontSize: 24 }}>
+      <Text style={{ fontFamily: "simonetta", fontSize: 24 }}>
         Fikrah Lejahtegis{"\n"}105841105322
       </Text>
 
-      <Text style={{ fontFamily: "roboto", fontSize: 24 }}>
+      <Text style={{ fontFamily: "sourgummy", fontSize: 24 }}>
         Alya Ananda{"\n"}105841105422
       </Text>
 
-      <Text style={{ fontFamily: "roboto-condensed", fontSize: 24 }}>
-        M fikri haikal ayatullah{"\n"}105841105522
+      <Text style={{ fontFamily: "sunshiney", fontSize: 24 }}>
+        M. Fikri Haikal Ayatullah{"\n"}105841105522
       </Text>
 
-      <Text style={{ fontFamily: "special-gothic", fontSize: 24 }}>
-        Muh Dirham Rahim{"\n"}105841105622
+      <Text style={{ fontFamily: "girlnextdoor", fontSize: 24 }}>
+        Muh. Dirham Rahim{"\n"}105841105622
       </Text>
 
-      <Text style={{ fontFamily: "ubuntu", fontSize: 24 }}>
-        Muh Ilham Akbar{"\n"}105841105722
+      <Text style={{ fontFamily: "tiltprism", fontSize: 24 }}>
+        Muh. Ilham Akbar{"\n"}105841105722
       </Text>
 
     </ScrollView>
